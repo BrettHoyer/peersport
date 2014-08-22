ce62b9eeac761dee05e35f2ac2c4f36d6599e32a
Brett Hoyer <jbh5079@gmail.com>
2014-08-22 00:40:34 -0700

    adding some sequalize basics and gitignore

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var Sequelize  = require('sequelize');
+var dbInfo = require('./env').database;
+var sequelize = new Sequelize(dbInfo.databaseName, dbInfo.username, dbInfo.password);
 
+var Test = sequelize.define('Test', {
+  name: { type: Sequelize.STRING }
+})
+
+Test.sync().success(function(){
+  console.log("CREATED DATABASE TABLE")
+})
+
+var test = Test.build({ name: "brett Hoyer" })
+
+test.save();
 
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
